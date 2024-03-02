b696ffa11b5227b6a8df0922d7ab5bc88166ef94
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2024-03-02 09:59:38 +0900

    MOD_キャラクターリストを配列化（選択できるように）

--- src/components/Charactar.tsx
+++ src/components/Charactar.tsx
@@ -1,22 +1,31 @@
 // import { useState } from 'react';
 
+const charactarList = [
+  { id: 1, name: 'マリオ', imageUrl: `${process.env.PUBLIC_URL}/fighter/mario.png` },
+  { id: 2, name: 'ドンキー', imageUrl: `${process.env.PUBLIC_URL}/fighter/donkey.png` },
+  { id: 3, name: 'サムス', imageUrl: `${process.env.PUBLIC_URL}/fighter/samus.png` },
+  { id: 4, name: 'ダムス', imageUrl: `${process.env.PUBLIC_URL}/fighter/samusd.png` },
+  { id: 5, name: 'ヨッシー', imageUrl: `${process.env.PUBLIC_URL}/fighter/yoshi.png` },
+  { id: 6, name: 'カービィ', imageUrl: `${process.env.PUBLIC_URL}/fighter/kirby.png` },
+  { id: 7, name: 'フォックス', imageUrl: `${process.env.PUBLIC_URL}/fighter/fox.png` },
+  { id: 8, name: 'ピカチュウ', imageUrl: `${process.env.PUBLIC_URL}/fighter/pikachu.png` },
+  { id: 9, name: 'ルイージ', imageUrl: `${process.env.PUBLIC_URL}/fighter/luigi.png` },
+  { id: 10, name: 'ネス', imageUrl: `${process.env.PUBLIC_URL}/fighter/ness.png` },
+  { id: 11, name: 'キャプテンファルコン', imageUrl: `${process.env.PUBLIC_URL}/fighter/captain.png` },
+  { id: 12, name: 'プリン', imageUrl: `${process.env.PUBLIC_URL}/fighter/purin.png` },
+  // { id: 13, name: 'ドンキー', imageUrl: `${process.env.PUBLIC_URL}/fighter/donkey.png` },
+  // { id: 14, name: 'ドンキー', imageUrl: `${process.env.PUBLIC_URL}/fighter/donkey.png` },
+  // { id: 15, name: 'ドンキー', imageUrl: `${process.env.PUBLIC_URL}/fighter/donkey.png` },
+]
+
 export const Charactar = () => {
   return (
     <>
       <div className="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div className="flex">
-          <img src={`${process.env.PUBLIC_URL}/fighter/mario.png`} alt="mario" />
-          <img src={`${process.env.PUBLIC_URL}/fighter/donkey.png`}  alt="donkey" />
-          <img src={`${process.env.PUBLIC_URL}/fighter/samus.png`}  alt="samus" />
-          <img src={`${process.env.PUBLIC_URL}/fighter/samusd.png`}  alt="samusd" />
-          <img src={`${process.env.PUBLIC_URL}/fighter/yoshi.png`}  alt="yoshi" />
-          <img src={`${process.env.PUBLIC_URL}/fighter/kirby.png`}  alt="kirby" />
-          <img src={`${process.env.PUBLIC_URL}/fighter/fox.png`}  alt="fox" />
-          <img src={`${process.env.PUBLIC_URL}/fighter/pikachu.png`}  alt="pikachu" />
-          <img src={`${process.env.PUBLIC_URL}/fighter/luigi.png`}  alt="luigi" />
-          <img src={`${process.env.PUBLIC_URL}/fighter/ness.png`}  alt="ness" />
-          <img src={`${process.env.PUBLIC_URL}/fighter/captain.png`}  alt="captain" />
-          <img src={`${process.env.PUBLIC_URL}/fighter/purin.png`}  alt="purin" />
+        {charactarList.map(character => (
+          <img src={character.imageUrl} alt={character.name} />
+        ))}          
         </div>
       </div>
     </>
