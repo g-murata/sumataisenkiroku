c0be08c7c16a87a7baef1a4fa0e2379200a9f738
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2024-03-02 19:59:07 +0900

    MOD_枠を常に表示。

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -12,24 +12,28 @@ export const Result: React.FC<ResultProps> = ({myWinCount, myLoseCount, results,
     <>
       <div className="">
         <h1>今日の戦績 {myWinCount}勝{myLoseCount}敗</h1>
-        {results.length > 0 &&
+        <div className="min-h-32 max-h-32 w-60 bg-white border border-neutral-800  overflow-y-auto hide-scrollbar">
+        {results.length > 0 ?
           <> 
-            <div className="bg-white border border-neutral-800 max-h-32 w-60 overflow-y-auto hide-scrollbar">
-              {results.map((result, index) => (
-                <div className={index === 0 && animateFirstItem ? "fadeIn" : ""} key={index}>
-                <div className="bg-white border border-neutral-400 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
-                    <div className="flex p-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                      <img src={`${process.env.PUBLIC_URL}${result.player.imageUrl}`} alt={result.player.name} />
-                      <label>VS</label>
-                      <img src={`${process.env.PUBLIC_URL}${result.opponentPlayer.imageUrl}`} alt={result.opponentPlayer.name} />
-                      <label >{result.shouhai}</label>
-                    </div>
+            {results.map((result, index) => (
+              <div className={index === 0 && animateFirstItem ? "fadeIn" : ""} key={index}>
+              <div className="bg-white border border-neutral-400 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+                  <div className="flex p-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <img src={`${process.env.PUBLIC_URL}${result.player.imageUrl}`} alt={result.player.name} />
+                    <label>VS</label>
+                    <img src={`${process.env.PUBLIC_URL}${result.opponentPlayer.imageUrl}`} alt={result.opponentPlayer.name} />
+                    <label >{result.shouhai}</label>
                   </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </>
+          :
+          <div className="h-32 flex justify-center items-center">
+            <h1 className="fadeIn text-red-500 font-bold">Ready to Fight!!</h1>
+          </div>
           }
+        </div>
       </div>
     </>
   )
