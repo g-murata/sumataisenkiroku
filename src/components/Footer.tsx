94f653273513ad71cc89224b32f214449c8c5712
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2024-03-02 12:24:54 +0900

    ADD_youtubeリンクの追加、サイトの説明修正等

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,9 @@ export const Footer = () => {
   return (
     <>
       <div className="flex flex-col">
+        <a href="https://www.youtube.com/@yarushikanai23" target="_blank" className="text-red-600 hover:text-red-800">
+          <i className="fab fa-youtube"></i> やるしかない！！
+        </a>
         <p>© 2024 yarushikanai!! all rights untara kantara</p>
       </div>
     </>
