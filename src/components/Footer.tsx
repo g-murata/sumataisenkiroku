debbd3a6c514071e65e4ecd97c2d68373578ef73
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2024-04-07 14:22:38 +0900

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,8 @@ export const Footer: React.FC<MyComponentProps> = ({ deleteMode, setdeleteMode }
         <a href="https://www.youtube.com/@yarushikanai23" rel="noopener noreferrer" target="_blank" className="text-red-600 hover:text-red-800">
           <i className="fab fa-youtube"></i> やるしかない！！
         </a>
-        <p>© 2024 yarushikanai!! all rights untara kantara</p>
+        <p className="text-gray-500">hon tool ha "maningen" ga sakusei shitayo.</p>
+        <p className="text-gray-500">© 2024 yarushikanai!! all rights untara kantara</p>
         <button className={`${deleteMode === true ? 'text-red-500' : 'text-blue-500'}`} onClick={() => setdeleteMode(!deleteMode)}>          
           {deleteMode === true ? '対戦結果削除モード ON': '対戦結果削除モード OFF'}
         </button>
