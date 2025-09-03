736d5b4d9683770378e343311f1727bea5505c44
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2025-09-04 08:27:17 +0900

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -106,7 +106,7 @@ export const Home = () => {
       <div className="flex flex-col justify-center items-center">
         <div className="md:flex">
           <div className="w-full md:w-1/3">
-            <div className= "px-5 py-2">
+            <div className= "px-5 py-2 flex flex-col justify-center items-center">
               <div>
                 <Charactar
                   player={"あなた"}
