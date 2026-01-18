1bc15a5f2f3d4c1f90dd3d372fbe7ae6d4711b79
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2026-01-18 19:47:37 +0900

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -61,7 +61,7 @@ export const Home: React.FC<HomeProps> = ({ history, onAddResult, onRowClick, on
       // @ts-ignore
       const win = await window.documentPictureInPicture.requestWindow({
         width: 350,
-        height: 400,
+        height: 200,
       });
 
       // ★★★ タイトル変更 ★★★
